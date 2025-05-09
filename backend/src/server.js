bc0b033a02bb5c52e85c9f10088e7c0da9aa9f89
Phát <125816527+AkaiShuichi711@users.noreply.github.com>
2025-05-09 08:55:30 +0700

    updated

---
+++ backend/src/server.js
@@ -0,0 +1,56 @@
+require('module-alias/register');
+const mongoose = require('mongoose');
+const { globSync } = require('glob');
+const path = require('path');
+
+// Make sure we are running node 7.6+
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
+if (major < 20) {
+  console.log('Please upgrade your node.js version at least 20 or greater. 👌\n ');
+  process.exit();
+}
+
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: '.env.local' });
+
+mongoose.connect(process.env.DATABASE);
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false
+  }
+);
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected'))
+  .catch(err => console.error('Unable to connect to database:', err));
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+mongoose.connection.on('error', (error) => {
+  console.log(
+    `1. 🔥 Common Error caused issue → : check your .env file first and add your mongodb url`
+  );
+  console.error(`2. 🚫 Error → : ${error.message}`);
+});
+
+const modelsFiles = globSync('./src/models/**/*.js');
+
+for (const filePath of modelsFiles) {
+  require(path.resolve(filePath));
+}
+
+// Start our app!
+const app = require('./app');
+app.set('port', process.env.PORT || 8888);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → On PORT : ${server.address().port}`);
+});
